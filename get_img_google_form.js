cbd82bea0e366c427fb594b03ecfc12ac8324294
Nguyen Khai Hoan <32768392+hoan111@users.noreply.github.com>
2021-10-22 01:29:15 +0700

    Update get_img_google_form.js

--- get_img_google_form.js
+++ get_img_google_form.js
@@ -1,18 +1,26 @@
 var x = document.getElementsByClassName('freebirdFormviewerViewItemsEmbeddedobjectImage');
+var y = document.getElementsByClassName('freebirdFormviewerComponentsQuestionBaseTitle');
 var jsonObj = [];
+data = {};
 var i;
 
 for (i = 0; i < x.length; i++) {
     var question_count = i + 1;
-    image = {};
-    image["Question_No"] = question_count;
-    image["img_url"] = x[i].currentSrc;
-    jsonObj.push(image);
+    data = {};
+    data["Question_No"] = question_count;
+    data["img_url"] = x[i].currentSrc;
+    jsonObj.push(data);
 }
+
+for (i = 0; i < y.length; i++) {
+    text = {};
+    text["Question_text"] = y[i].innerText;
+    jsonObj.push(text);
+}
+
 jsonStr = JSON.stringify(jsonObj, null, 2);
 copyToClipboard(jsonStr);
 console.log("Đã copy dữ liệu vào bộ nhớ!");
-
 function copyToClipboard(text) {
     var dummy = document.createElement("textarea");
 
